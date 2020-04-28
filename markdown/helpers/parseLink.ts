9e624bb7cf13d11e4a99ae47109e11b41ebf9efe
SteroideBello <36568899+SteroideBello@users.noreply.github.com>
2020-04-28 17:39:10 +0200

    Add files via upload

---
+++ markdown/helpers/parseLink.ts
@@ -0,0 +1,20 @@
+import type { ParseFunction } from "simple-markdown"
+import { depunycode } from "./depunycode"
+
+export const parseLink: ParseFunction = capture => {
+  const [, content] = capture
+
+  const url = depunycode(content)
+
+  if (!url) {
+    return {
+      type: "text",
+      content,
+    }
+  }
+
+  return {
+    content: [{ type: "text", content: url }],
+    target: url,
+  }
+}
