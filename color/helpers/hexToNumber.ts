f7301d939d3ab38210c83fa8804ba8ab79bd2fff
SteroideBello <36568899+SteroideBello@users.noreply.github.com>
2020-04-28 17:33:44 +0200

    Add files via upload

---
+++ color/helpers/hexToNumber.ts
@@ -0,0 +1,1 @@
+export const hexToNumber = (hex: string) => Number.parseInt(hex.slice(1), 16)
---
+++ color/helpers/numberToHex.ts
@@ -0,0 +1,4 @@
+import type { DiscordColor } from "../types/DiscordColor"
+
+export const numberToHex = (number: DiscordColor): string =>
+  typeof number === "number" ? `#${number.toString(16).padStart(6, "0")}` : ""
